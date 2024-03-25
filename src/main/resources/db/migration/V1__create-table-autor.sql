485ada8b34564fb40773646c587582bd29cc8f3f
rafao <rafael.chuchon@sptech.school>
2024-03-24 21:22:07 -0300

    modificação em migrations adicionada

--- src/main/resources/db/migration/V1__create-table-autor.sql
+++ src/main/resources/db/migration/V1__create-table-autor.sql
@@ -1,17 +1,16 @@
 create table autor(
 
-    id bigint not null auto_increment,
+    id bigint not null auto_increment primary key,
     nome varchar(100) not null,
-    email varchar(100) not null unique,
     data_nascimento date not null,
-    cpf varchar(11) not null unique,
-    logradouro varchar(100) not null,
-    bairro varchar(100) not null,
-    cep varchar(9) not null,
-    complemento varchar(100),
+    nacionalidade varchar(100) not null,
+    genero_literario varchar(100) not null,
+    email varchar(100) not null unique,
+    endereco_postal varchar(100) not null,
+    telefone varchar(11) not null,
     numero varchar(20),
-    uf char(2) not null,
-    cidade varchar(100) not null,
-
-    primary key(id)
+    preferencia_de_contato varchar(100),
+    rede_social varchar(200),
+    site varchar(100),
+    disponibilidade_de_eventos boolean not null
 );
