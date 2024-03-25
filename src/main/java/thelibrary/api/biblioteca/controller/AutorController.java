ab162dc97ccd869cd915cb94b4bd7f9e81a0afa6
rafao <rafael.chuchon@sptech.school>
2024-03-25 01:26:55 -0300

    Properties e Pageable atualizados e funcionais

--- src/main/java/thelibrary/api/biblioteca/controller/AutorController.java
+++ src/main/java/thelibrary/api/biblioteca/controller/AutorController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import thelibrary.api.biblioteca.dto.autor.DadosCadastroAutor;
 import thelibrary.api.biblioteca.dto.autor.DadosConsultaAutor;
@@ -35,8 +36,14 @@ public class AutorController  {
 //        return listagem;
         return repository.findAll().stream().map(DadosConsultaAutor::new).toList();
     }
-    @GetMapping("/paginacao")
+    @GetMapping("/paginacao") // Se achar nescessário pode realizar mais consultas relativas a isto no site do Spring Data JPA procurando por Pageable.
     public Page<DadosConsultaAutor> listar(Pageable paginacao){
         return repository.findAll(paginacao).map(DadosConsultaAutor::new);
     }
+
+    @GetMapping("/ordenado") // Neste exemplo usa-se o @PageableDefault para ordenar por nome. Ele é uma anotação que serve para definir valores padrão para a paginação.
+    public Page<DadosConsultaAutor> buscar(@PageableDefault(sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosConsultaAutor::new);
+    }
+
 }
