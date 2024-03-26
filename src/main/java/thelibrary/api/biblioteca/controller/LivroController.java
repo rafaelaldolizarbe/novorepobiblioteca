e0799c8e6eb256016f65b88ffdfd8f35d461d906
rafao <rafael.chuchon@sptech.school>
2024-03-26 20:50:12 -0300

    adicionado migration de livros e Post

--- src/main/java/thelibrary/api/biblioteca/controller/LivroController.java
+++ src/main/java/thelibrary/api/biblioteca/controller/LivroController.java
@@ -1,18 +1,31 @@
 package thelibrary.api.biblioteca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import thelibrary.api.biblioteca.dto.livro.DadosCadastrarLivro;
+import thelibrary.api.biblioteca.entity.Livro;
+import thelibrary.api.biblioteca.repository.livro.LivroRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastrarLivro livro){
-        System.out.println(livro);
+
+        livroRepository.save(new Livro(livro));
     }
 
     @GetMapping
-    public String cumprimentos(){
-        return "Está funcionando";
+    public List<Livro> listartudo(){
+
+        return livroRepository.findAll();
     }
+
+
 }
--- src/main/java/thelibrary/api/biblioteca/entity/Livro.java
+++ src/main/java/thelibrary/api/biblioteca/entity/Livro.java
@@ -7,6 +7,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.ISBN;
+import thelibrary.api.biblioteca.dto.livro.DadosCadastrarLivro;
 import thelibrary.api.biblioteca.entity.enumerable.GeneroLiterario;
 
 
@@ -21,12 +22,18 @@ public class Livro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "autor_id")
-    private Autor autor;
+    private String autor;
     private Integer anoPublicacao;
     private String editora;
     private GeneroLiterario genero;
     private String isbn;
 
+    public Livro(DadosCadastrarLivro livro) {
+        this.titulo = livro.titulo();
+        this.autor = livro.autor();
+        this.anoPublicacao = livro.anoPublicacao();
+        this.editora = livro.editora();
+        this.genero = livro.genero();
+        this.isbn = livro.isbn();
+    }
 }
